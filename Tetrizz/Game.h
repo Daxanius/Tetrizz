13e04ba38551aa382254476134026d4b3faff1ff
Daxanius <balder.huybreghs@gmail.com>
2023-12-06 15:38:47 +0100

    Add all tetrimino shapes

--- Tetrizz/Game.h
+++ Tetrizz/Game.h
@@ -3,19 +3,35 @@
 #include "Playfield.h"
 #include "Tetrimino.h"
 
-
-
 #pragma region ownDeclarations
-// Constants
+// CONSTANTS
+static const float RGB_MAX{ 255.f };
 
+// Shapes
+static const Point2f L_SHAPE[]{ { -1, 0 },  { 0,  0 }, { 1,  0 }, { 2, 0 } };
+static const Point2f O_SHAPE[]{ { 0,  0 },  { 1,  0 }, { 0,  1 }, { 1, 1 } };
+static const Point2f T_SHAPE[]{ { 0,  0 },  { 0,  1 }, { -1, 1 }, { 1, 1 } };
+static const Point2f J_SHAPE[]{ { 0,  0 },  { -1, 0 }, { 0,  1 }, { 0, 2 } };
+static const Point2f L_SHAPE[]{ { 0,  0 },  { 1,  0 }, { 0,  1 }, { 0, 2 } };
+static const Point2f S_SHAPE[]{ { 0,  0 },  { -1, 0 }, { 0,  1 }, { 1, 1 } };
+static const Point2f Z_SHAPE[]{ { 0,  0 },  { -1, 1 }, { 0,  1 }, { 1, 0 } };
 
+static const Tetrimino TETRIMINOS_ARR[] {
+  Tetrimino(L_SHAPE, Color4f{0   / RGB_MAX, 253 / RGB_MAX, 255 / RGB_MAX}),
+  Tetrimino(O_SHAPE, Color4f{255 / RGB_MAX, 254 / RGB_MAX, 64  / RGB_MAX}),
+  Tetrimino(T_SHAPE, Color4f{255 / RGB_MAX, 42  / RGB_MAX, 248 / RGB_MAX}),
+  Tetrimino(J_SHAPE, Color4f{0   / RGB_MAX, 25  / RGB_MAX, 248 / RGB_MAX}),
+  Tetrimino(J_SHAPE, Color4f{255 / RGB_MAX, 192 / RGB_MAX, 35  / RGB_MAX}),
+  Tetrimino(S_SHAPE, Color4f{0   / RGB_MAX, 252 / RGB_MAX, 61  / RGB_MAX}),
+  Tetrimino(Z_SHAPE, Color4f{255 / RGB_MAX, 35  / RGB_MAX, 18  / RGB_MAX})
+};
+
+// GLOBALS
 Playfield* m_PlayfieldPtr;
-static const Point2f L_SHAPE[4]{ {-1 , 0 } , { 0 , 0 } , { 1 , 0 } , { 2 ,0 } };
-static const Tetrimino TETRIMINOS_ARR[] {Tetrimino(L_SHAPE , Color4f{0.2f ,0.5f,0.9f} ) } ;
+int g_TickCount{};
 
 // Declare your own functions here
 // Texture g_Texture;
-int g_TickCount{};
 
 #pragma endregion ownDeclarations
 
--- Tetrizz/Tetrimino.h
+++ Tetrizz/Tetrimino.h
@@ -1,22 +1,21 @@
 #pragma once
 
+static const int  MINO_COUNT{ 4 };
+
 class Tetrimino
 {
-
 private:
-	static const int  MINOS_COUNT{4};
-	Point2f m_MinosArr[MINOS_COUNT];
+	Point2f m_MinosArr[MINO_COUNT];
 	Color4f m_MinosColor{};
 
 public:
 	Point2f* GetMinos();
 	Color4f GetColor();
-	int GetMinosCount();
 
 	void Rotate();
 
 	Tetrimino(Tetrimino* tetrimino);
-	Tetrimino(const Point2f minosArr[MINOS_COUNT], Color4f minosColor);
+	Tetrimino(const Point2f minosArr[MINO_COUNT], Color4f minosColor);
 	~Tetrimino();
 };    
 
