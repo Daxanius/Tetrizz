10f8935ec25dd035fc342fd92286e80f08e46f78
Kwanton <quentibiewgmail.com>
2023-12-07 12:02:00 +0100

    Functions in Playfield

--- Tetrizz/Playfield.cpp
+++ Tetrizz/Playfield.cpp
@@ -25,27 +25,42 @@ const int Playfield::GetScore() const
 
 void Playfield::MoveLeft()
 {
-
+	m_Playstate.fieldPosition.x -= 1;
 }
 
 void Playfield::MoveRight()
 {
-
+	m_Playstate.fieldPosition.x += 1;
 }
 
 void Playfield::MoveDown()
 {
-
+	m_Playstate.fieldPosition.y += 1;
 }
 
 void  Playfield::Rotate()
 {
-
+	if (CanRotate())
+	{
+		m_Playstate.currentTetriminoPtr->Rotate();
+	}
 }
 
 void  Playfield::SaveTetromino()
 {
+	if (m_Playstate.currentTetriminoPtr != nullptr)
+	{
+		Tetromino* currentPtr{m_Playstate.currentTetriminoPtr};
+		Tetromino* SavedPtr{m_Playstate.savedTetriminoPtr};
 
+		m_Playstate.currentTetriminoPtr = SavedPtr;
+		m_Playstate.savedTetriminoPtr = currentPtr;
+	}
+	else
+	{
+		m_Playstate.savedTetriminoPtr = m_Playstate.currentTetriminoPtr;
+		m_Playstate.currentTetriminoPtr = m_Playstate.nextTetriminoPtr;
+	}
 }
 
 void Playfield::QuickPlace()
